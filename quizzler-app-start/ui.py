62f6abd9c6075335e0d5e8bac377b08f2224e9b0
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-09-23 06:09:54 +0100

    Add files via upload

---
+++ quizzler-app-start/ui.py
@@ -0,0 +1,74 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(pady=20, padx=20, background=THEME_COLOR)
+
+        self.score_number = 0
+        self.score_label = Label(text=f"Score: {self.score_number}", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, background="white")
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="", font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
+
+        false_png = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_png, command=self.is_false)
+        self.false_button.grid(column=0, row=2)
+
+        true_png = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_png, command=self.is_true)
+        self.true_button.config(highlightthickness=0)
+        self.true_button.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def is_true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+        self.get_next_question()
+
+    def is_false(self):
+        user_answer = "False"
+        if self.quiz.check_answer(user_answer):
+            self.score_number += 1
+        self.get_next_question()
+
+    def give_feedback(self, is_true):
+        self.window.after(500, func=self.right_feedback)
+
+    def right_feedback(self):
+        self.canvas.config(background="green")
+
+    def wrong_feedback(self):
+        self.canvas.config(background="red")
+
+    # def interface(self):
+    #     score_label = Label(text=f"Score: ")
+    #     score_label.grid(column=1, row=0)
+    #
+    #     canvas = Canvas(width=300, height=250, background="white")
+    #     canvas.create_text(text="", font=("Arial", 20, "italic"))
+    #     canvas.grid(column=0, row=1, columnspan=2)
+    #
+    #     false_png = PhotoImage(file="./images/false.png")
+    #     false_button = Button(image=false_png)
+    #     false_button.grid(column=0, row=2)
+    #
+    #     true_png = PhotoImage(file="./images/true.png")
+    #     true_button = Button(image=true_png)
+    #     true_button.grid(column=1, row=2)
+
+
